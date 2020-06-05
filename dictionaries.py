afae6e43b117e7ae7ec2d34e2ad6ab5d1b3b0e38
WailGree <szilagyi.roland.dominik@gmail.com>
2020-06-05 11:48:34 +0200

    implemented function

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,12 @@
+def merge_dicts(dict_one, dict_two):
+    # Flake8 won't cry because of the first one. For second one it will.
+    # {dict_two.update({key: value}) if key not in dict_two.keys() else dict_two.update({key: (dict_two[key] + dict_one[key])}) for key, value in dict_one.items()}
+    # for key, value in dict_one.items(): dict_two.update({key: value}) if key not in dict_two.keys() else dict_two.update({key: (dict_two[key] + dict_one[key])})
+    {dict_two.update({key: value}) if key not in dict_two.keys() else dict_two.update(
+        {key: (dict_two[key] + dict_one[key])}) for key, value in dict_one.items()}
+    return dict_two
+
+
 dict_one = {'a': 1, 'b': 2, 'c': 3}
 dict_two = {'d': 1, 'e': 2, 'f': 3, 'a': 99, }
-# Flake8 won't cry because of the first one. For second one it will.
-# {dict_two.update({key: value}) if key not in dict_two.keys() else dict_two.update({key: (dict_two[key] + dict_one[key])}) for key, value in dict_one.items()}
-# for key, value in dict_one.items(): dict_two.update({key: value}) if key not in dict_two.keys() else dict_two.update({key: (dict_two[key] + dict_one[key])})
-print(dict_two)
+print(merge_dicts(dict_one, dict_two))
